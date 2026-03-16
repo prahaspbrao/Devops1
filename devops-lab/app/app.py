26e9260c186cbac046317135dd2ab921ae9bb53e
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-03-16 12:19:58 +0530

    Docker + Git working properly

---
+++ devops-lab/app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import redis, os
+import socket
+
+app = Flask(__name__)
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=6379,
+    decode_responses=True
+)
+
+@app.route("/")
+def index():
+    count = r.incr("visits")
+    return jsonify({
+        "message": "Hello from the DevOps Lab!",
+        "hostname": socket.gethostname(),
+        "visits": count
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
